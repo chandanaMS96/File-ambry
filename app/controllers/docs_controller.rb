211d0cda929350a409cf939efe7b6de02335c2f8
chandana <chandanams96@gmail.com>
2019-06-17 11:00:18 +0530

    created Docs controller and added resources routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,32 @@
+class DocsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+  end
+
+end
